583fb4fb4ed1ce404765bfbab95d6f76108aa610
AidanH567 <herstikaidan@gmail.com>
2025-12-15 20:22:18 +0100

    edits to css

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function Header() {
     return (
-        <header>
+        <header className="header">
             <Link to="/" className="header-logo">
                 #VanLife
             </Link>
