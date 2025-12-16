1fe9fc900b1321078610a87137b7b7487b472e4f
AidanH567 <herstikaidan@gmail.com>
2025-12-16 17:48:44 +0100

    added photo

--- Pages/About.jsx
+++ Pages/About.jsx
@@ -1,8 +1,13 @@
 import React from "react"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 export default function About() {
     return (
-        <h1>About Page</h1>
+        <>
+        <Header></Header>
+        <Footer></Footer>
+        </>
     )
 }
 
