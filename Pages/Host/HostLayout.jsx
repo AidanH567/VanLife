be13b7cee4c8e22543d0e4746a348b52db4b2c75
AidanH567 <herstikaidan@gmail.com>
2025-12-18 11:48:29 +0100

    changing layouts

--- Pages/Host/HostLayout.jsx
+++ Pages/Host/HostLayout.jsx
@@ -1,10 +1,15 @@
 import {Outlet} from "react-router-dom"
 import Header from "../../components/Header"
+import { Link } from "react-router-dom"
 
 export default function Layout() {
     return (
         <>
-            <h1>Layout Component</h1>
+            <nav>
+                <Link to="/host">Dashboard</Link>
+                <Link to="/host/income">Income</Link>
+                <Link to="/host/reviews">Reviews</Link>     
+            </nav>
             <Outlet/>
         </>
     )
