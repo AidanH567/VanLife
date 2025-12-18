0968f9c65162c84b0a06e769d40d812733c6267a
AidanH567 <herstikaidan@gmail.com>
2025-12-18 09:26:44 +0100

    added routes

---
+++ Pages/Host/Reviews.jsx
@@ -0,0 +1,3 @@
+export default function Reviews() {
+    return <h1>This is the Reveiws Page</h1>
+}
--- index.jsx
+++ index.jsx
@@ -8,27 +8,33 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import { makeServer } from "./server"
 import VanDetail from './Pages/VanDetail';
+import Layout from './components/Layout';
+import Dashboard from './Pages/Host/Dashboard';
+import Income from './Pages/Host/Income';
+import Reviews from './Pages/Host/Reviews';
 
 makeServer()
 
 function App() {
   return (
-    <>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-      <Route path="/vans" element={<Vans/>}></Route>
-      <Route path="/vans/:id" element={<VanDetail/>}></Route>
-    </Routes>
-    <Footer/>
-    </>
+    <BrowserRouter>
+      <Routes >
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/host" element={<Dashboard />}></Route>
+          <Route path="/host/income" element={<Income />}></Route>
+          <Route path="/host/reviews" element={<Reviews />}></Route>
+          <Route path="/vans" element={<Vans />}></Route>
+          <Route path="/vans/:id" element={<VanDetail />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>);
+    <App />
+  );
