50f0e94d03cdd088bf04ecad73c15568d5c8f1ed
AidanH567 <herstikaidan@gmail.com>
2026-01-12 17:01:51 +0100

    edits

--- Pages/About.jsx
+++ Pages/About.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import { Link } from "react-router-dom"
 
-export default function About() {
+export default function About() {   
     return (
         <>
            
